da5402a0b5f94b4f49a23bda6c384f47bcca859b
Kristan <kjvilla23@yahoo.com>
2022-03-06 12:15:45 -0600

    Added login logic

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -22,14 +22,16 @@ const {isAuthenticated, logout} = useAuth();
                     <li class="py-8 px-4 hover:cursor-pointer hover:bg-purple-500 hover:text-purple-800">Login
                     </li>
                     </router-link>
-                    <router-link v-else :to="{name: 'Secret'}">
+                    <div v-else class="flex">
+                        <router-link :to="{name: 'Secret'}">
                     <li class="py-8 px-4 hover:cursor-pointer hover:bg-purple-500 hover:text-purple-800">Secret
                     </li>
                     </router-link>
-                    <button v-else@click="logout">
+                    <button @click="logout">
                         <li class="py-8 px-4 hover:cursor-pointer hover:bg-purple-500 hover:text-purple-800">Logout
                     </li>
                     </button>
+                    </div>
                 </ul>
             </nav>
         </div>
--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,12 +1,34 @@
+<script setup>
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import useAuth from "../useAuth";
+
+const { isAuthenticated, login } = useAuth();
+
+const username = ref("");
+const password = ref("");
+
+const router = useRouter(); 
+
+const logginIn = () => {
+    login(username.value, password.value);
+    if (isAuthenticated.value) {
+        router.push("/");
+    }
+};
+</script>
+
 <template>
     <div class ="flex flex-col space-y-12 items-center justify-center min-h-screen-nonav">
-        <h2 class="text-6xl tracking-tighter font-extralight">Login</h2> 
+        Logged in: {{isAuthenticated}} 
         <div class="flex items-center justify-center bg-gray-200 overflow-hidden rounded-lg shadow-2xl">
             <img class="h-64" src="../assets/bglogin.png" alt="Hello BG" >
-            <form class="flex flex-col space-y-4 p-4">
-                <input type="text" class="p-2 border-2 rounded-lg" placeholder="Username">
-                <input type="password" class="p-2 border-2 rounded-lg" placeholder="Password">
-                <button type="submit" class="py-2 rounded-lg bg-purple-500 font-bold" >Login</button>
+            <form @submit.prevent="logginIn" class="flex flex-col space-y-4 p-4">
+                <input type="text" class="p-2 border-2 rounded-lg" placeholder="Username"
+                v-model="username">
+                <input type="password" class="p-2 border-2 rounded-lg" placeholder="Password"
+                v-model="password">
+                <button type="submit" @submit.prevent="logginIn" class="py-2 rounded-lg bg-purple-500 font-bold" >Login</button>
                 </form>  
         </div>
     </div>
